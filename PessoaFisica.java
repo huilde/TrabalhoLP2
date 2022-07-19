e32a911e46b11bd126b9e92cb432309ac52bb27f
João Filipe Silva <joao.filipe.nascimento.016@ufrn.edu.br>
2022-07-19 10:44:54 -0300

    Add files via upload

--- PessoaFisica.java
+++ PessoaFisica.java
@@ -1,12 +1,13 @@
 package com.example.bla.pessoa;
 
 import java.time.LocalDate;
-
+/*
 public class PessoaFisica extends Pessoa {
 
-    public PessoaFisica(String _nome, String _sobrenome) {
-        super(String _nome, String _sobrenome);
+    public PessoaFisica(String nome, String sobrenome) {
+        super(String nome, String sobrenome);
     }
     public String cpf;
     public LocalDate dataNascimento;
 }
+*/
