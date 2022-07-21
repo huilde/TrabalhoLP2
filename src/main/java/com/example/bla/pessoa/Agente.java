70e184d34305d9151d7f4e17281c9e377fd13495
Huilde Viana Junior <hvianajunior@apple.com>
2022-07-21 19:18:24 -0300

    docs: adicionando documentação nas classes agente e funcionário

--- src/main/java/com/example/bla/pessoa/Agente.java
+++ src/main/java/com/example/bla/pessoa/Agente.java
@@ -2,6 +2,21 @@ package com.example.bla.pessoa;
 
 public class Agente extends Funcionario {
 
+ /**
+     * Construtor do Funcionari
+     * @param nome  nome do funcionário
+     * @param sobrenome   sobrenome do funcionário
+     * @param cpf cpf do funcionário
+     *  @param salario salario do funcionário
+     */
+    public Agente(String nome, String sobrenome, String cpf, double salario) {
+        super(nome, sobrenome, cpf, salario);
+    }
+
+ /**
+     * Método responsável por vender as passagens
+     */
+
     public void venderPassagem() {
         System.out.println("passagem vendida");
     }
--- src/main/java/com/example/bla/pessoa/Funcionario.java
+++ src/main/java/com/example/bla/pessoa/Funcionario.java
@@ -1,11 +1,41 @@
 package com.example.bla.pessoa;
 
 public abstract class Funcionario extends Pessoa {
-
-    public void darInformacao() {
-        System.out.println("info");
+    private double salario;
+    
+     /**
+     * Construtor do Funcionario
+     *
+     * @param nome  nome do funcionário
+     * @param sobrenome   sobrenome do funcionário
+     * @param cpf cpf do funcionário
+     *  @param salario salario do funcionário
+     */
+    public Funcionario(String nome, String sobrenome, String cpf, double salario) {
+        super(nome, sobrenome, cpf);
+        this.salario = salario;
     }
 
+    /**
+     * getter de salario
+     *  @returns salario do funcionário
+     */
+
+    public double getSalario() {
+        return salario;
+    }
+
+    /**
+     * setter de salario
+     */
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+/**
+     * função respomsável por fazer o funcionario tomar café
+     */
     public void tomarCafe() {
         System.out.println("quem recusa um bom cafe?");
     }
