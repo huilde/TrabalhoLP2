8ec57483ecd626f04aba4fc127c8bd537b51410d
João Filipe Silva <joao.filipe.nascimento.016@ufrn.edu.br>
2022-07-21 19:04:15 -0300

    Update Motorista.java

--- src/main/java/com/example/bla/pessoa/Motorista.java
+++ src/main/java/com/example/bla/pessoa/Motorista.java
@@ -1,7 +1,19 @@
 package com.example.bla.pessoa;
 
+/**
+ * Classe Motorista
+ *
+ * Herda da classe Funcionario.
+ * Representa o funcionário do tipo motorista
+*/
+
 public class Motorista extends Funcionario {
 
+/**
+ * Ação de dirigir
+ *
+ * @return vazio
+*/
     public void dirigir() {
         System.out.println("dirigindo...");
     }
