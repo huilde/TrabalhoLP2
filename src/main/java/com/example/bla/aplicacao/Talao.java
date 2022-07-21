b9b23da182ca273b21f3cd316fcd15d12f5afee3
João Filipe Silva <joao.filipe.nascimento.016@ufrn.edu.br>
2022-07-21 18:38:53 -0300

    Update Talao.java

--- src/main/java/com/example/bla/aplicacao/Talao.java
+++ src/main/java/com/example/bla/aplicacao/Talao.java
@@ -4,12 +4,21 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Classe que representa um talão de controle das passagens de ônibus
+ */
+
 public class Talao {
 
     private String id;
     private LocalDate dataImpressao;
     private List<String> passagens = new ArrayList<>();
-
+    
+/**
+ * Construtor do Talao
+ * @param id    identificador do talão
+ * @param dataImpressao     data da impressão do talão
+*/
     Talao(String id, LocalDate dataImpressao) {
         this.id = id;
         this.dataImpressao = dataImpressao;
