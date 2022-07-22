56ef69cd44c290c9dbb491e816ef112d9286f926
João Filipe Silva <joao.filipe.nascimento.016@ufrn.edu.br>
2022-07-21 21:25:27 -0300

    Update Onibus.java

--- src/main/java/com/example/bla/aplicacao/Onibus.java
+++ src/main/java/com/example/bla/aplicacao/Onibus.java
@@ -38,51 +38,96 @@ public class Onibus {
         this.linha = linha;
         this.motorista = motorista;
     }
+    
+ /**
+ * getter de motorista
+ * 
+ * @return motorista
+ */
 
     public Motorista getMotorista() {
         return motorista;
     }
-
+/**
+ * setter de motorista
+ * modifica o nome do motorista
+ */
     public void setMotorista(Motorista motorista) {
         this.motorista = motorista;
     }
-
+    
+ /**
+ * getter de id
+ * 
+ * @return id do ônibus
+ */
     public Integer getId() {
         return id;
     }
-
+/**
+ * setter de id
+ * modifica o id do ônibus
+ */
     public void setId(Integer id) {
         this.id = id;
     }
-
+/**
+ * getter de situação
+ * 
+ * @return situação do ônibus
+ */
     public Situacao getSituacao() {
         return situacao;
     }
-
+/**
+ * setter de situação
+ * modifica a situação do ônibus
+ */
     public void setSituacao(Situacao situacao) {
         this.situacao = situacao;
     }
-
+/**
+ * getter de próxima manutenção do ônibus
+ * 
+ * @return próxima manutenção do ônibus
+ */
     public LocalDate getProximaManutencao() {
         return proximaManutencao;
     }
-
+/**
+ * setter de próxima manutenção do ônibus
+ * modifica a próxima manutenção do ônibus
+ */
     public void setProximaManutencao(LocalDate proximaManutencao) {
         this.proximaManutencao = proximaManutencao;
     }
-
+/**
+ * getter de frequência de manutenção do ônibus
+ * 
+ * @return frequência de manutenção do ônibus
+ */
     public Period getFrequenciaManutencao() {
         return frequenciaManutencao;
     }
-
+/**
+ * setter de frequência de manutenção do ônibus
+ * modifica a frequência de manutenção do ônibus
+ */
     public void setFrequenciaManutencao(Period frequenciaManutencao) {
         this.frequenciaManutencao = frequenciaManutencao;
     }
-
+/**
+ * getter de linha
+ * 
+ * @return linha do ônibus
+ */
     public Linha getLinha() {
         return linha;
     }
-
+/**
+ * setter de linha
+ * modifica a linha do ônibus
+ */
     public void setLinha(Linha linha) {
         this.linha = linha;
     }
