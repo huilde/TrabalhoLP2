8c15c960d02270d3aae495fff930b1940bcce38d
João Filipe Silva <joao.filipe.nascimento.016@ufrn.edu.br>
2022-07-21 21:36:52 -0300

    Update Passageiro.java

--- src/main/java/com/example/bla/pessoa/Passageiro.java
+++ src/main/java/com/example/bla/pessoa/Passageiro.java
@@ -4,21 +4,36 @@ import java.util.ArrayList;
 
 import com.example.bla.aplicacao.Passagem;
 
+/**
+ * Classe Passageiro que herda da classe Pessoa
+ * Representa o passageiro do ônibus
+ * @param saldo     saldo do passageiro
+ * @param passagens     lista de passagens de ônibus
+ */
+
 public class Passageiro extends Pessoa {
 
     private double saldo;
 
     private ArrayList<Passagem> passagens = new ArrayList<>();
-
+    
+/**
+ * Construtor do passageiro
+ */
     Passageiro(String nome, String sobrenome, double saldo) {
         super(nome, sobrenome, "");
         this.saldo = saldo;
     }
-
+/**
+ * Recarrega do saldo do passageiro
+ */
     public void fazerRecarga(double valor) {
         saldo = saldo + valor;
     }
-
+    
+/**
+ * Compra passagem
+ */
     public void comprarPassagem(Passagem passagem) {
         if (saldo < passagem.getValor()) {
             System.out.println("Você não possui saldo suficiente para comprar essa passagem");
